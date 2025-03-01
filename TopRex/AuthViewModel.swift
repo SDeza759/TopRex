ba12d2e37aaa3f2602204f81709dee6bb6b65e4c
SDeza759 <sebastian.deza7@gmail.com>
2025-03-01 12:28:53 -0500

    debugging loggedin status

--- TopRex/AuthViewModel.swift
+++ TopRex/AuthViewModel.swift
@@ -11,12 +11,17 @@ import SwiftUI
 
 class AuthViewModel: ObservableObject {
     @Published var isLoggedIn: Bool = Auth.auth().currentUser != nil
+    init() {
+        self.isLoggedIn = Auth.auth().currentUser != nil
+        print("DEBUG: Initial Auth State - isLoggedIn: \(isLoggedIn)")
+    }
 
     func signOut() {
         do {
             try Auth.auth().signOut()
             DispatchQueue.main.async {
                 self.isLoggedIn = false
+                print("DEBUG: User signed out. isLoggedIn is now \(self.isLoggedIn)")
             }
         } catch {
             print("Error signing out: \(error.localizedDescription)")
